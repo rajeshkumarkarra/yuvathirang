fff564a77e6482c8ab17d00d475401aa113adc3f
rajeshkumarkarra <rajeshkumarkarra@outlook.com>
2019-07-05 12:45:03 +0530

    removed collectstatic

--- yuvathirang/settings.py
+++ yuvathirang/settings.py
@@ -118,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
+'''STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
-)
+)'''
 STATIC_URL = '/static/'
 django_heroku.settings(locals())
